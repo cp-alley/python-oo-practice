96f4304212114dd23d79ad0ad511a1edefe798f7
Steven Dowd <swdowd@gmail.com>
2023-08-22 18:30:29 -0400

    Implemented serial and began work on wordfinder

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
-    """Machine to create unique incrementing serial numbers.
-    
+    """Machine to create unique incrementing serial numbers
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,23 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start = 100):
+        """Create a serial number generator starting from a provided input num"""
+        self.start = self.current = start
+
+    def __repr__(self):
+        return f"Series starting with {self.start}. Current number is {self.current}."
+
+    def generate(self):
+        """Returns the next number in the sequence that begins with the start value"""
+        self.current+=1
+        return self.current - 1
+
+    def reset(self):
+        """Resets the generator to the original start value"""
+        self.current = self.start
+
+
+
+
+
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,30 @@
+from random import random
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, path):
+        """A word finder for a provided file."""
+        self.path = path
+        self.word_list = []
+        # call method to print num of words read
+        self.read_file()
+
+    def __repr__(self):
+        return f"A word finder for the file at {self.path}"
+
+    def read_file(self):
+        """Reads the provided file and notes how many words were read. Called upon
+        initialization of an intance."""
+        file = open(self.path, "r")
+        count = 0
+        for line in file:
+            count+=1
+            self.word_list.append(line.strip('\n'))
+        print(f"{count} words read")
+
+    def random(self):
+        """Returns a random wors from the words the finder read from the provided
+        file."""
+        ran_num = int(random.random() * len(self.word_list))
+        return self.word_list[ran_num]
+
