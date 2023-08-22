47a5dc437bc83e7a38780e4c99ebf2d629463180
Christopher Alley <calleyviola@gmail.com>
2023-08-22 19:36:03 -0400

    Work on special word finder

--- wordfinder.py
+++ wordfinder.py
@@ -8,6 +8,7 @@ class WordFinder:
         self.word_list = []
         # call method to print num of words read
         self.read_file()
+        self.print_count()
 
     def __repr__(self):
         return f"A word finder for the file at {self.path}"
@@ -16,15 +17,24 @@ class WordFinder:
         """Reads the provided file and notes how many words were read. Called upon
         initialization of an intance."""
         file = open(self.path, "r")
-        count = 0
         for line in file:
-            count+=1
             self.word_list.append(line.strip('\n'))
-        print(f"{count} words read")
 
-    def random(self):
+    def print_count(self):
+        """Print the number of words read"""
+        print(f"{len(self.word_list)} words read")
+
+    def random_word(self):
         """Returns a random wors from the words the finder read from the provided
         file."""
-        ran_num = int(random.random() * len(self.word_list))
+        ran_num = int(random() * len(self.word_list))
+        print(ran_num)
         return self.word_list[ran_num]
 
+class SpecialWordFinder(WordFinder):
+    """Finds words from files that include comments and blank lines"""
+    super().read_file()
+
+    def read_file(self):
+        self.word_list = [word for word in self.word_list if not word == ""]
+        self.word_list = [word for word in self.word_list if not word[0] == "#"]
