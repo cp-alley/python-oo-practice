10201ea7427d1ce52a375e015f9ba77e0ab2f0db
Steven Dowd <swdowd@gmail.com>
2023-08-22 20:14:36 -0400

    Completed and refactored WordFinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,24 +1,25 @@
-from random import random
+from random import choice
 
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self, path):
         """A word finder for a provided file."""
         self.path = path
-        self.word_list = []
+        self.word_list = self.read_file()
         # call method to print num of words read
-        self.read_file()
         self.print_count()
 
     def __repr__(self):
         return f"A word finder for the file at {self.path}"
 
     def read_file(self):
+        #DONE: return a list and set word_list equal to that val in init
         """Reads the provided file and notes how many words were read. Called upon
         initialization of an intance."""
         file = open(self.path, "r")
-        for line in file:
-            self.word_list.append(line.strip('\n'))
+        new_list = [line.strip() for line in file]
+        return new_list
+
 
     def print_count(self):
         """Print the number of words read"""
@@ -27,14 +28,14 @@ class WordFinder:
     def random_word(self):
         """Returns a random wors from the words the finder read from the provided
         file."""
-        ran_num = int(random() * len(self.word_list))
-        print(ran_num)
-        return self.word_list[ran_num]
+        #DONE: use random's choice function, import choice instead
+        return choice(self.word_list)
 
 class SpecialWordFinder(WordFinder):
     """Finds words from files that include comments and blank lines"""
-    super().read_file()
 
     def read_file(self):
-        self.word_list = [word for word in self.word_list if not word == ""]
-        self.word_list = [word for word in self.word_list if not word[0] == "#"]
+        """An updated read_file method that strips out comments and empty lines"""
+        #DONE: use word.startswith()
+        return [word for word in super().read_file()
+                          if not word == "" and not word.startswith("#") ]
